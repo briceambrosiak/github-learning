ff457c92f9183efe6b9ef19954110ece70ad09eb
Brice Ambrosiak <bambrosiak@pictet-technologies.com>
2025-07-14 11:18:44 +0200

    Build CI

--- training/src/main/java/ptl/GithubTrainingMain.java
+++ training/src/main/java/ptl/GithubTrainingMain.java
@@ -2,6 +2,6 @@ package ptl;
 
 public class GithubTrainingMain {
     public static void main(String[] args) {
-        System.out.println("Hello, GitHub Training!");
+        System.out.println("Hello, GitHub Training!!");
     }
 }
